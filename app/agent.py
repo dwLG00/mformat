8cfa9691246e8a061c2587b3672aebcba2e027db
dwLG00 <wallace.dylan00@gmail.com>
2025-10-28 17:36:09 -0700

    more fix

--- app/agent.py
+++ app/agent.py
@@ -23,11 +23,10 @@ class MangaFormatAgent:
             messages=self.memory,
             tools=self.sandbox_env.as_tools()
         )
-        tool_calls = response.choices[0].message.tool_calls
+        rmessage = response.choices[0].message
+        tool_calls = rmessage.tool_calls
         while tool_calls:
-            if print_responses: 
-                if response_message := response.choices[0].message.content:
-                    print(response_message)
+            if print_responses and rmessage.content: print(rmessage.content)
             for tool_call in tool_calls:
                 tool_call_id = tool_call.id
                 function_name = tool_call.function.name
@@ -35,8 +34,8 @@ class MangaFormatAgent:
                 if print_responses: print(f"[$] {function_name}({', '.join(f'{key}={repr(value)}' for key, value in args.items())})")
                 tool_call_output = self.sandbox_env.run_tool(function_name, args)
                 tool_call_response = {
-                    "type": "function_call",
-                    "call_id": tool_call_id,
+                    "role": "tool",
+                    "tool_call_id": tool_call_id,
                     "output": tool_call_output
                 }
                 self.memory.append(tool_call_response)
@@ -45,3 +44,5 @@ class MangaFormatAgent:
                 messages=self.memory,
                 tools=self.sandbox_env.as_tools()
             )
+            rmessage = response.choices[0].message
+            tool_calls = rmessage.tool_calls
